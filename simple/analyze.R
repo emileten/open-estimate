52f0679e99da03b68bab9ec532a0e80b5b4c60d0
James Rising <jarising@gmail.com>
2014-09-02 14:48:43 -0400

    add stata analysis

--- simple/analyze.R
+++ simple/analyze.R
@@ -1,9 +1,10 @@
 setwd("~/projects/dmas/simple")
 
+source("tools_reg.R")
 tbl <- read.csv("data.csv")
 
 mod <- lm(satell ~ width + factor(color), data=tbl)
 
-source("tools_reg.R")
-
+summary(mod)
+get.coef.robust(mod)
 get.coef.clust(mod, tbl$color)
