558ca29a09845627938845f941a26d17246e2e33
James Rising <jarising@gmail.com>
2015-07-02 12:46:06 -0700

    add polynomial creation

--- models/generate.py
+++ models/generate.py
@@ -29,3 +29,20 @@ def uniform_constant(xx, yy, min, max):
 
     return table
 
+def polynomial(lowbound, highbound, betas, covas, num=40):
+    betas = np.array(betas)
+    covas = np.mat(covas)
+
+    if covas.shape[0] != covas.shape[1] and len(betas) != covas.shape[0]:
+        return "Error: Please provide a complete covariance matrix."
+    if np.any(linalg.eig(covas)[0] < 0):
+        return "Error: Covariance matrix is not positive definite."
+
+    xx = np.linspace(lowbound, highbound, num=num)
+    xxs = {}
+    for x in xx:
+        xvec = np.mat([[1, x, x**2, x**3][0:len(betas)]])
+        serr = np.sqrt(xvec * covas * np.transpose(xvec))
+        xxs[x] = (betas.dot(np.squeeze(np.asarray(xvec))), serr[0,0])
+        
+    return SplineModel.create_gaussian(xxs, xx)
