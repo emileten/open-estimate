88e208cb7798d7548c14200ee4377aa504bafa11
James Rising <jarising@gmail.com>
2016-02-27 15:35:44 -0800

    working swap model

--- src/swapbin/swapmodel.py
+++ src/swapbin/swapmodel.py
@@ -1,34 +1,70 @@
 import numpy as np
 import transform
 from ..models.spline_model import SplineModel
+from ..models.bin_model import BinModel
 
-def spline_find_bins(model, beta, vcv):
+def swap_any(model, beta, vcv, dropbin, totals):
+    if isinstance(model, SplineModel):
+        return swap_spline(model, beta, vcv, dropbin, totals)
+    if isinstance(model, BinModel) and isinstance(model.model, SplineModel):
+        return swap_bin(model, beta, vcv, dropbin, totals)
+
+    raise NotImplementedError("Do not know how to swap bin for " + str(model))
+
+def find_bins(means, sdevs, beta, vcv):
     bins = []
-    for x in model.get_xx():
-        mean = model.get_mean(x)
-        sdev = model.get_sdev(x)
+    for ii in range(len(means)):
+        if np.isnan(means[ii]):
+            continue
 
-        mean_errors = (np.array(beta) - mean) ** 2
-        sdev_errors = (np.array(np.sqrt(vcv.diag())) - sdev) ** 2
+        mean_errors = (np.array(beta) - means[ii]) ** 2
+        sdev_errors = (np.sqrt(np.array(vcv).diagonal()) - sdevs[ii]) ** 2
         errors = mean_errors + sdev_errors
         
         index = np.argmin(errors)
-        if errors[index] < 1e-6 * mean:
+        if errors[index] < 1e-6 * means[ii]:
             bins.append(index)
         else:
-            raise LookupError("Cannot find " + str(mean) + " +- " + str(sdev))
+            raise LookupError("Cannot find " + str(means[ii]) + " +- " + str(sdevs[ii]))
 
     return bins
 
-def spline_swap(model, beta, vcv, dropbin, totals):
-    # Find the bins in beta and vcv
-    bins = find_bins(model, beta, vcv)
-    # Transform the entire beta and vcv matrix
-    T = transform(len(beta), bins, dropbin, totals)
-    
-    vcv2 = swap_vcv(vcv, T)
-    beta2 = swap_beta(beta, T)
+def swap_spline(model, beta, vcv, dropbin, totals):
+    means = [model.get_mean(x) for x in model.get_xx()]
+    sdevs = [model.get_sdev(x) for x in model.get_xx()]
+    means2, sdevs2 = swap_values(means, sdevs, beta, vcv, dropbin, totals)
 
     # Construct a new model
-    xxs = {model.get_xx()[ii]: (beta2[bins[ii]], np.sqrt(vcv2[bins[ii], bins[ii]])) for ii in range(len(bins))}
-    SplineModel.create_gaussian(xxs, model.xx_is_categorical)
+    jj = 0 # index into xx (goes 1 ahead at dropped bin)
+    xxs = {}
+    order = []
+    for ii in range(len(means)):
+        x = model.get_xx()[ii]
+        order.append(x)
+        if np.isnan(means[ii]):
+            xxs[x] = (np.nan, np.nan)
+        else:
+            xxs[x] = (means2[jj], sdevs2[jj])
+            jj += 1
+
+    print order
+    print xxs
+    return SplineModel.create_gaussian(xxs, order=order, xx_is_categorical=model.xx_is_categorical)
+
+def swap_bin(model, beta, vcv, dropbin, totals):
+    model2 = swap_spline(model.model, beta, vcv, dropbin, totals)
+    return BinModel(model.get_xx(), model2)
+
+def swap_values(means, sdevs, beta, vcv, dropbin, totals):
+    # Find the bins in beta and vcv
+    bins = find_bins(means, sdevs, beta, vcv)
+    # Transform the entire beta and vcv matrix
+    T = transform.transform(len(beta), bins, dropbin, totals)
+    
+    vcv2 = transform.swap_vcv(vcv, T)
+    beta2 = transform.swap_beta(beta, T)
+
+    means2 = [beta2[ii] for ii in bins]
+    sdevs2 = [np.sqrt(vcv2[ii, ii]) for ii in bins]
+
+    return means2, sdevs2
