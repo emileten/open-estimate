8af543411b831b0ddfb7d8898a1bc54d04abb1e7
James Rising <jarising@gmail.com>
2016-03-06 17:06:37 -0800

    make an installable library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='openest',
+      version='0.1',
+      description='Library of empirical model application.',
+      url='http://github.com/jrising/open-estimate',
+      author='James Rising',
+      author_email='jarising@gmail.com',
+      license='GNU v. 3',
+      packages=['openest'],
+      zip_safe=False)
