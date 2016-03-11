f3fbaeb82f41e0b42667898cec50a63fb4890e74
James Rising <jarising@gmail.com>
2016-03-11 09:03:48 -0500

    add montecarlo approach

---
+++ openest/lincombo/montecarlo.py
@@ -0,0 +1,18 @@
+from scipy.stats import norm
+import statsmodels.api as sm
+
+def regress_draws(means, serrs, XX, count=1000):
+    betas = []
+    for ii in range(count):
+        yy = norm.rvs(means, serrs, 1)
+        betas.append(sm.OLS(yy, XX).fit().params)
+
+    return np.array(betas)
+
+def regress_summary(means, serrs, XX, count=1000):
+    betas = regress_draws(means, serrs, XX, count=count)
+    beta_mean = np.mean(betas, axis=0)
+    beta_serr = np.std(betas, axis=0)
+
+    return beta_mean, beta_serr
+
