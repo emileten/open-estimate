9281548142c3c8166981b8cf065b809a6d4ce276
James Rising <jarising@gmail.com>
2017-03-20 11:05:36 -0700

    fix unit test

--- openest/test/models/test_curve.py
+++ openest/test/models/test_curve.py
@@ -5,4 +5,4 @@ def test_CubicSplineCurve():
     knots = [-12, -7, 0, 10, 18, 23, 28, 33]
     curve = CubicSplineCurve(knots, np.zeros(len(knots) - 1))
     print curve.get_terms(20)
-    np.testing.assert_equal(curve.get_terms(20), [32768, 19683, 8000, 1000, 8, 0])
+    np.testing.assert_equal(curve.get_terms(20), [20, 32768, 19683, 8000, 1000, 8, 0])
