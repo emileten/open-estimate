d0fa13ef1976d6e365c40c6a85c339e31f221172
James Rising <jarising@gmail.com>
2017-05-02 14:54:26 -0700

    add dmas access functions

---
+++ openest/dmas/remote.py
@@ -0,0 +1,25 @@
+def view_model(objtype, id):
+  if objtype == 'url':
+    while True:
+      print "Accessing url..."
+      try:
+        return StoredModel.create_from_url(id, (float('-inf'), float('inf')))
+      except:
+        time.sleep(1)
+        pass
+
+  if objtype == 'model':
+    return MetaModel.get_model(id, False)
+
+  if objtype == 'collection':
+    collection = Collection.get(id, False)
+
+    models = []
+    for meta in collection.metas:
+      models.append(meta.model())
+
+    return Model.merge(models)
+
+  if objtype == 'view':
+    view = ModelView.get(id)
+    return CombinationController.view_model(view.objtype, view.objid)
---
+++ openest/dmas/server.py
@@ -0,0 +1,27 @@
+import urllib2
+
+# The domain we include in emails and such
+public_domain = "dmas.berkeley.edu"
+
+def domain_url(location):
+    if location[0] != '/':
+        location = '/' + location
+    return 'http://' + public_domain + location
+
+def open_url(url):
+    """
+    Returns an open file to the URL.
+    """
+    if len(url) < 7 or url[0:7] != "http://":
+        url = domain_url(url)
+
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none',
+           'Accept-Language': 'en-US,en;q=0.8'}
+
+    req = urllib2.Request(url, headers=hdr)
+    return urllib2.urlopen(req)
+
+
