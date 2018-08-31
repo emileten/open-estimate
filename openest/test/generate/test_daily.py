bf864af71a30f861545ed73ff84fe5798ecc61c1
James Rising <jarising@gmail.com>
2018-08-31 11:08:16 -0400

    another 10

--- openest/test/generate/test_daily.py
+++ openest/test/generate/test_daily.py
@@ -8,14 +8,14 @@ test_model = BinModel([-40, 0, 80], SplineModel.create_gaussian({0: (0, 1), 1: (
 
 def test_YearlyDayBins():
     application = YearlyDayBins(test_model, 'days').test()
-    for (year, result) in application.push(DailyWeatherSlice(1800 * 100 + np.arange(365), np.array([-10] * 300 + [10] * 65), manyregion=False)):
+    for (year, result) in application.push(DailyWeatherSlice(1800 * 1000 + np.arange(365), np.array([-10] * 300 + [10] * 65), manyregion=False)):
         np.testing.assert_equal(result, 65.)
-    for (year, result) in application.push(DailyWeatherSlice(2000 * 100 + np.arange(365), np.array([-10] * 65 + [10] * 300), manyregion=False)):
+    for (year, result) in application.push(DailyWeatherSlice(2000 * 1000 + np.arange(365), np.array([-10] * 65 + [10] * 300), manyregion=False)):
         np.testing.assert_equal(result, 300.)
 
 def test_Constant():
     application = Constant(33, 'widgets').test()
-    for (year, result) in application.push(DailyWeatherSlice(1800 * 100 + np.arange(365), np.random.rand(365), manyregion=False)):
+    for (year, result) in application.push(DailyWeatherSlice(1800 * 1000 + np.arange(365), np.random.rand(365), manyregion=False)):
         np.testing.assert_equal(result, 33)
-    for (year, result) in application.push(DailyWeatherSlice(2000 * 100 + np.arange(365), np.random.rand(365), manyregion=False)):
+    for (year, result) in application.push(DailyWeatherSlice(2000 * 1000 + np.arange(365), np.random.rand(365), manyregion=False)):
         np.testing.assert_equal(result, 33)
